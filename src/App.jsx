8192ff93557ead83fa93cddb436f0d517605b20f
Aung Khaing Khant <aungkhaingkhant.dev@gmail.com>
2025-01-13 23:34:43 +0630

    Rooms are displayed with a color-code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rooms, setRooms] = useState([]);
 
+  // Using useEffect for data fetching
+  useEffect(() => {
+    axios.get('http://localhost:8080/all-rooms')
+      .then(response => {
+        setRooms(response.data); // Update the state with the fetched data
+      })
+      .catch(error => {
+        console.log(error); // Log errors (optional)
+      });
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+  const getColor = (status) => {
+    if(status === 'AVAILABLE'){
+      return 'green';
+    }else if(status === 'UNDER_MAINTENANCE'){
+      return 'red';
+    }else{
+      return 'yellow';
+    }
+  }
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <center><h3>Rooms</h3></center>
+        <div style={{display: 'flex', flexWrap: 'wrap' /* center the div */ ,justifyContent: 'center'}}>
+          {rooms.map(room => (
+            <div key = {room.id} style={{height:'100px',width:'300px',border: `2px solid ${getColor(room.roomStatus.status)}`, margin: '10px',padding:'5px'}}>
+              <h3>Room Number: {room.roomNo}</h3>
+              <p>Price: {room.roomType.price}</p>  
+            </div>
+          ))}
+        </div>
+        </div>
+  );
 }
 
-export default App
+export default App;
